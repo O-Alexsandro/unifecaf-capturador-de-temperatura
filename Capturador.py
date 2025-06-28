98e8ae2c08fad2b290d845d5964d68bff692940e
O-Alexsandro <alexsandrooliveiraribas@gmail.com>
2025-06-28 18:31:40 -0300

    adição do tkinter

--- Capturador.py
+++ Capturador.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,38 +11,53 @@ from datetime import datetime
 #options = Options()
 #options.add_experimental_option("detach", True)
 
-navegador = webdriver.Chrome()
+def executar_coleta():
 
-#Abre o navegador e pesquisa o site de clima
-navegador.get('https://www.climatempo.com.br/previsao-do-tempo/agora/cidade/558/saopaulo-sp')
+    navegador = webdriver.Chrome()
 
-# Pegar temperatura
-temperatura = navegador.find_element(By.XPATH, '//*[@id="mainContent"]/div[6]/div[3]/div[1]/div[3]/div[1]/div/div[2]/span').text
+    #Abre o navegador e pesquisa o site de clima
+    navegador.get('https://www.climatempo.com.br/previsao-do-tempo/agora/cidade/558/saopaulo-sp')
 
-# Pegar sensação
-sensacao = navegador.find_element(By.XPATH, '//*[@id="mainContent"]/div[6]/div[3]/div[1]/div[3]/div[1]/div/div[3]/span').text
+    # Pegar temperatura
+    temperatura = navegador.find_element(By.XPATH, '//*[@id="mainContent"]/div[6]/div[3]/div[1]/div[3]/div[1]/div/div[2]/span').text
 
-# Pegar umidade
-umidade = navegador.find_element(By.XPATH, '//*[@id="mainContent"]/div[6]/div[3]/div[1]/div[3]/div[1]/div/div[4]/ul/li[2]/div[2]/p/span').text
+    # Pegar sensação
+    sensacao = navegador.find_element(By.XPATH, '//*[@id="mainContent"]/div[6]/div[3]/div[1]/div[3]/div[1]/div/div[3]/span').text
 
-agora = datetime.now()
-data = agora.strftime("%d/%m/%Y")
-hora = agora.strftime("%H:%M:%S")
+    # Pegar umidade
+    umidade = navegador.find_element(By.XPATH, '//*[@id="mainContent"]/div[6]/div[3]/div[1]/div[3]/div[1]/div/div[4]/ul/li[2]/div[2]/p/span').text
 
-#Abre o execel
-arquivo = load_workbook('temperaturas-salvas.xlsx')
+    agora = datetime.now()
+    data = agora.strftime("%d/%m/%Y")
+    hora = agora.strftime("%H:%M:%S")
 
-plan = arquivo['Clima']
+    #Abre o execel
+    arquivo = load_workbook('temperaturas-salvas.xlsx')
 
-# Descobre a próxima linha vazia
-ultima_linha = plan.max_row + 1
+    plan = arquivo['Clima']
 
-# Preenche os dados na próxima linha
-plan.cell(row=ultima_linha, column=1).value = temperatura
-plan.cell(row=ultima_linha, column=2).value = sensacao
-plan.cell(row=ultima_linha, column=3).value = umidade
-plan.cell(row=ultima_linha, column=4).value = data
-plan.cell(row=ultima_linha, column=5).value = hora
+    # Descobre a próxima linha vazia
+    ultima_linha = plan.max_row + 1
 
-arquivo.save('temperaturas-salvas.xlsx')
-print('Planilha atualizada com sucesso!')
+    # Preenche os dados na próxima linha
+    plan.cell(row=ultima_linha, column=1).value = temperatura
+    plan.cell(row=ultima_linha, column=2).value = sensacao
+    plan.cell(row=ultima_linha, column=3).value = umidade
+    plan.cell(row=ultima_linha, column=4).value = data
+    plan.cell(row=ultima_linha, column=5).value = hora
+
+    arquivo.save('temperaturas-salvas.xlsx')
+    print('Planilha atualizada com sucesso!')
+
+    navegador.close()
+
+def executar_coleta_thread():
+    Thread(target=executar_coleta).start()
+
+root = tk.Tk()
+root.title("Capturador de Temperatura")
+
+btn = tk.Button(root, text="Pesquisar a temperatura atual", command=executar_coleta_thread)
+btn.pack(padx=20, pady=20)
+
+root.mainloop()
